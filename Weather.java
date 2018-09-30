17383a5893f51d23d2ad009ba2ca3966727deb6f
mahesh-mogilisetty <43693201+mahesh-mogilisetty@users.noreply.github.com>
2018-09-29 20:07:54 -0700

    Update Weather.java

--- Weather.java
+++ Weather.java
@@ -15,25 +15,25 @@ import org.json.JSONObject;
 public class Weather {
 
 	public static void main(String[] args) throws IOException {
-		String input;
+		//String input;
 		try{
-		System.out.println("tomorrow predicted temperature");
+		System.out.println("*****Temperature Predictor****");
 		System.out.println("Enter the 5 digit Zip code of the city in USA");
 		BufferedReader br =  new BufferedReader(new InputStreamReader(System.in));
 		
-		while((input=br.readLine())!=null && !input.equals("exit") && !input.equals("quit")) {
+		//while((input=br.readLine())!=null && !input.equals("exit") && !input.equals("quit")) {
 			try {
-			int zipcode = 75252;
-			zipcode = Integer.parseInt(input);
-			getDetails(zipcode);
+				int zipcode = 75252;
+				zipcode = Integer.parseInt(br.readLine());
+				getDetails(zipcode);
 			}catch (NumberFormatException e) {
 				System.out.println("Please enter a valid zip code\n");
 			}
 			
-			System.out.println("Want to check temperature at another location,\nthen please Enter the 5 digit Zip code of the city in USA or enter exit to quit");
+			//System.out.println("Want to check temperature at another location,\nthen please Enter the 5 digit Zip code of the city in USA or enter exit to quit");
 			
-		}
-		System.out.println(".....Program Exited.....");
+		//}
+		//System.out.println(".....Program Exited.....");
         
 		}catch (IOException e) {
 			e.printStackTrace();
@@ -44,11 +44,8 @@ public class Weather {
 public static void getDetails(int zipcode) {
 		
 		try {
-		    // Make a URL to the web page
-        // You can generate the APP_ID and APP_CODE after signing up on "https://developer.here.com" 
-        // metric = false, Temperature would be in fahrenheit (For Celcius type metric = true)
-        // reports back hourly forecast for the next 7 days
-        URL url = new URL("https://weather.api.here.com/weather/1.0/report.json?app_id={Your_APP_ID}&app_code={Your_APP_CODE}&product=forecast_hourly&metric=false&zipcode=" + zipcode);
+		// Make a URL to the web page
+        URL url = new URL("https://weather.api.here.com/weather/1.0/report.json?app_id={YOUR_APP_ID}&app_code={YOUR_APP_CODE}&product=forecast_hourly&metric=false&zipcode=" + zipcode);
         url.openStream().close();
         // Get the input stream through URL Connection
         URLConnection con = url.openConnection();
@@ -73,7 +70,9 @@ public static void getDetails(int zipcode) {
 		int i = 0;
 		String today = arr.getJSONObject(i).getString("weekday");
 		for (i=1;today.equals(arr.getJSONObject(i).getString("weekday"));i++)
-		{}
+		{
+		}
+		//System.out.println("i = " + i); 
 		for (int j=0; j < 24; i++,j++)
 		{
 			temp.add(Double.parseDouble((arr.getJSONObject(i).getString("temperature"))));
@@ -83,21 +82,16 @@ public static void getDetails(int zipcode) {
 		String time = dateTime.get(min_index).substring(0,2);
 		String date = dateTime.get(min_index).substring(2, dateTime.get(min_index).length());
 		date = date.substring(0,2) + "-" + date.substring(2,4) + "-" + date.substring(4,8);
-		String meridiem = "am";
-		//Display time in meridiem
-		if(Integer.parseInt(time) > 11)
-		{
-			if(time!="12")
-				time = Integer.toString((Integer.parseInt(time) - 12));
-			meridiem = "pm";
-		} else {
-			if(time=="00")
-				time = "12";
-		}
-		//Finds the coolest hour of tomorrow for the city of user feeded zipcode
-		System.out.println("coolest hour in " + cityName + " tomorrow on " + date + " would be at " + time + " " + meridiem);
-		System.out.println("and the temperature would be " + temp.get(min_index) +  " F\n");
 		
+		//System.out.println("coolest hour in " + cityName + " tomorrow would be at " + dateTime.get(min_index));
+		System.out.println("coolest hour in " + cityName + " tomorrow on " + date + " would be at " + AM_PM(time));
+		System.out.println("and the temperature would be " + temp.get(min_index) +  " F");
+		
+		System.out.println("Time\tTemperature");
+		for(int j=0; j<24; j++)
+		{
+			System.out.println(AM_PM(dateTime.get(j).substring(0,2)) + "\t  " + temp.get(j) + " F");
+		}
 		
 	}catch (IOException e) {
 		System.out.println("City not found, please enter a valid Zip Code\n");
@@ -106,6 +100,22 @@ public static void getDetails(int zipcode) {
 		System.out.println(" Invalid API format ");
 	}
 
+}
+
+public static String AM_PM(String time) {
+	
+	String meridiem = "am";
+	//System.out.println("time = " + time);
+	if(Integer.parseInt(time) > 11)
+	{
+		if(!time.equals("12"))
+			time = Integer.toString((Integer.parseInt(time) - 12));
+		meridiem = "pm";
+	} else {
+		if(time.equals("00"))
+			time = "12";
+	}
+	return time+" "+meridiem;
 }
 }
 
